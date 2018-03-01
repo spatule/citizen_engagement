962080e1b814288dc06a486d7988bad09ef5704a
Cédric Schär <cedric.schaer@heig-vd.ch>
2018-03-01 09:03:10 +0100

    Delete function implemented

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('../app').mangoose;
+var debug = require('debug')('users');
+var mongoose = require('mongoose');
 var app = require('../app');
 var User = require("../models/user");
+const ObjectId = mongoose.Types.ObjectId;
 
 /* GET users listing. */
 
 router.get('/:user_id', function (req, res, next) {
     User.findOne({"_id": req.params.user_id}, function (error, user_found) {
         if (error) {
+            // err status 404
             res.send(app.generateJsonErrorMessage("The user with id " + req.params.user_id + " could not be found."));
         } else {
             res.send(user_found);
@@ -47,7 +50,27 @@ router.patch('/:user_id', function (req, res, next) {
 });
 
 router.delete('/:user_id', function (req, res, next) {
-    res.send('USER 1');
+    const userId = req.params.user_id;
+    User.findOne({"_id": userId}, function (error, user_found) {
+        if (error) {
+            if (ObjectId.isValid(userId)) {
+                res.send(error);
+            } else {
+                next(error);
+            }
+        } else if (user_found) {
+            user_found.remove(function(error) {
+                if (error) {
+                    return next(error);
+                }
+                debug(`Deleted user "${user_found.firstName},${user_found.lastName}"`);
+                res.sendStatus(204);
+            });
+        } else {
+            res.status(404);
+            res.send(app.generateJsonErrorMessage("The user with id " + req.params.user_id + " could not be found."));
+        }
+    });
 });
 
 module.exports = router;
