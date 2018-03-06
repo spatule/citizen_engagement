adcb6726e1f877f529508824aedcc53c0493c17f
spat <philippe.spat@heig-vd.ch>
2018-03-06 15:24:20 +0100

    updated delete route with correct reponse messages

--- routes/issues.js
+++ routes/issues.js
@@ -5,23 +5,22 @@ var app = require('../app');
 var Issue = require("../models/issue");
 
 // Middleware
-var findIssueFromParam = function(req, res, next){
-  Issue.findOne({"_id": req.params.issue_id}, function (error, issue_found) {
-    if (error) {
-      if(mongoose.Types.ObjectId.isValid(req.params.issue_id)){
-          res.status(500).send(app.generateJsonErrorMessage("Unexpected error with Database"));
-      } else {
-        res.status(404).send(app.generateJsonErrorMessage("The issue with id " + req.params.issue_id + " could not be found (Id not valid)."));
-      }
+var findIssueFromParam = function (req, res, next) {
+    Issue.findOne({"_id": req.params.issue_id}, function (error, issue_found) {
+        if (error) {
+            if (mongoose.Types.ObjectId.isValid(req.params.issue_id)) {
+                res.status(500).send(app.generateJsonErrorMessage("Unexpected error with Database"));
+            } else {
+                res.status(404).send(app.generateJsonErrorMessage("The issue with id " + req.params.issue_id + " could not be found (Id not valid)."));
+            }
 
-    } else if(issue_found === null){
-      res.status(404).send(app.generateJsonErrorMessage("The issue with id " + req.params.issue_id + " could not be found."));
-    }
-    else {
-    req.issue = issue_found;
-    next();
-  }
-  });
+        } else if (issue_found === null) {
+            res.status(404).send(app.generateJsonErrorMessage("The issue with id " + req.params.issue_id + " could not be found."));
+        } else {
+            req.issue = issue_found;
+            next();
+        }
+    });
 };
 /* GET issues listing. */
 /**
@@ -42,8 +41,8 @@ var findIssueFromParam = function(req, res, next){
  * @apiSuccess {String} Issue.status The status of the issue
  * @apiError IssueNotFound The issue with <code>id</code> could not be found.
  */
-router.get('/:issue_id', findIssueFromParam,  function(req, res, next) {
-        res.send(req.issue);
+router.get('/:issue_id', findIssueFromParam, function (req, res, next) {
+    res.send(req.issue);
 });
 /**
  * @api {get} /issues?user=:user_id Request the issue's of the user
@@ -63,31 +62,31 @@ router.get('/:issue_id', findIssueFromParam,  function(req, res, next) {
  * @apiSuccess {String} Issue.status The status of the issue
  * @apiError IssueNotFound The issue with <code>id</code> could not be found.
  */
-router.get('/', function(req, res, next) {
-  Issue.find({}, function (error, issues_from_user) {
-      if (error) {
-          res.send(app.generateJsonErrorMessage("The issue with id " + req.query.issue_id + " could not be found."));
-      } else {
-          var issueMap = {};
-          issues_from_user.forEach(function (issue) {
-            if(issue.user==req.query.user){
-              issueMap[issue._id] = issue;
-            }
-          });
-          res.send(issueMap);
-      }
-  });
+router.get('/', function (req, res, next) {
+    Issue.find({}, function (error, issues_from_user) {
+        if (error) {
+            res.send(app.generateJsonErrorMessage("The issue with id " + req.query.issue_id + " could not be found."));
+        } else {
+            var issueMap = {};
+            issues_from_user.forEach(function (issue) {
+                if (issue.user == req.query.user) {
+                    issueMap[issue._id] = issue;
+                }
+            });
+            res.send(issueMap);
+        }
+    });
 });
 
 router.post('/', function (req, res, next) {
-  var issue = new Issue(req.body);
-  issue.save(function (error, saved_issue) {
-      if (error) {
-          res.send(error);
-      } else {
-          res.send(saved_issue);
-      }
-  });
+    var issue = new Issue(req.body);
+    issue.save(function (error, saved_issue) {
+        if (error) {
+            res.send(error);
+        } else {
+            res.send(saved_issue);
+        }
+    });
 });
 
 router.patch('/:issue_id', function (req, res, next) {
@@ -100,18 +99,11 @@ router.patch('/:issue_id', function (req, res, next) {
     });
 });
 
-router.delete('/:issue_id',  function (req, res, next) {
-
-  Issue.findByIdAndRemove(req.params.issue_id, (err, issue_removed) => {
-    // As always, handle any potential errors:
-    if (err) return res.status(500).send(err);
-    // We'll create a simple object to send back with a message and the id of the document that was removed
-    // You can really do this however you want, though.
-    const response = {
-        message: "Issue successfully deleted",
-        id: issue_removed._id
-    };
-    return res.status(200).send(response);
-});
+router.delete('/:issue_id', function (req, res, next) {
+    Issue.findByIdAndRemove(req.params.issue_id, (err, issue_removed) => {
+        if (err)
+            return res.status(500).send(app.generateJsonErrorMessage("Unable to delete the issue with ID " + issue_removed._id));
+        return res.status(200).send(issue_removed);
+    });
 });
 module.exports = router;
